22d0419c4eac5c4d1797dc21991fb4a004e21ba9
Alessio Montuoro <alessio@montuoro.info>
2020-11-12 20:42:52 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="fileargs",        
-    version="0.1",
+    version="0.2",
     author="AlessioM",
     author_email="none@example.com",
     long_description=long_description,
