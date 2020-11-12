a9e1db78b0774422c8ce2c9d5fb88802be43ad5f
Alessio Montuoro <alessio@montuoro.info>
2020-11-12 20:38:19 +0100

    fixed imports and dependencies

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name="fileargs",
+    name="fileargs",        
     version="0.1",
     author="AlessioM",
     author_email="none@example.com",
-    url="",
-    packages=find_namespace_packages(include=['fileargs.*']),
-    package_dir={'':'src'},
-    install_requires=[
-        "click>=7.1.2",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/AlessioM/fileargs",
+    packages=find_packages(),    
+    install_requires=[        
         "Jinja2>=2.11.2",
         "PyYAML>=5.3.1", 
         "configparser>=5.0.1"       
     ],
-    extras_require={},
-    setup_requires=[],
     python_requires='>=3.7',
-    test_suite='tests',
-    tests_require=[],
-    zip_safe=False,
-    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],    
     entry_points='''
         [console_scripts]
         fileargs=fileargs.command_line:main        
